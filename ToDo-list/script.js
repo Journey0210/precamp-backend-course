8f7151703e879aeb4321a4659a78e2225a1ca1dc
Hyejin <whgpwls210@naver.com>
2023-11-22 17:28:50 +0900

    update to-do-list

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,27 +1,61 @@
 let todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
 
-const createTodo = () => {
-  const todoList = document.querySelector("#todo-list");
+const savedTodoList = JSON.parse(localStorage.getItem("savedItems"));
+
+if (savedTodoList) {
+  // console.log(savedTodoList);
+  savedTodoList.forEach((ele) => createTodo(ele));
+}
+
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.todo;
+  }
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItems();
   });
 
-  newSpan.textContent = todoInput.value;
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
   todoInput.value = "";
-  console.log(newLi);
+  saveItems();
 };
 
 const keyCodeCheck = () => {
-  //   console.log();
-  //Enter event
-
   if (window.event.keyCode === 13 && todoInput.value !== "") {
     createTodo();
   }
+};
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  console.log(liList);
+  liList.forEach((ele) => ele.remove());
+};
+
+const saveItems = () => {
+  const liList = todoList.childNodes;
+
+  let items = [];
+  liList.forEach((ele, idx) =>
+    items.push({
+      key: idx + 1,
+      todo: ele.childNodes[1].textContent,
+      complete: ele.classList.contains("complete"),
+    })
+  );
+  localStorage.setItem("savedItems", JSON.stringify(items));
+  console.log(JSON.stringify(items));
 };
